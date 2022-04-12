cc887c738d681f0a64bfddd138f40979d19e6b0a
iliashe <schestakov.ilia@gmail.com>
2022-04-12 23:42:05 +0200

    day.js is updated

--- src/store/day.js
+++ src/store/day.js
@@ -1,12 +1,47 @@
+// USE TESTS(MOCHA) TO CHECK THE DATA
+// GENERATE AN ARRAY OF MONTH OBJECTS
+
+// function that returns an array of months objects
+const months = function(year) {
+  // init array, that will be returned
+  const arr = [];
+  // init month interval
+  let monthInt = [0, 0];
+  // looping through months
+  for(let month = 1; month <= 12; month += 1) {
+    // relative to number of day in a year (1st, 42nd, 349th etc), 
+    // current month starts on a day, when prev month ends.  
+    monthInt[0] = monthInt[1];
+    monthInt[1] += new Date(year, month, 0).getDate();
+    // getting name of a month
+    monthName = Intl.DateTimeFormat('en-US', { month: 'long' }).format(new Date(year, month, 0));
+    // adding a month object to the array
+    arr.push(
+      {
+        interval: [
+          monthInt[0],
+          monthInt[1]
+        ],
+        name: monthName,
+      }
+    );
+  }
+  return arr;
+}
+
 export default class Day {
   constructor(props) {
     this.configs = props.configs;
-    this.dateInMonth = props.dateInMonth;
+    this.dayInYear = props.dayInYear;
     this.events = props.events;
     this.isCurrent = props.isCurrent;
     this.isSelected = props.isSelected;
     this.month = props.month;
     this.name = props.name;
     this.year = props.year;
+    // this.week
   }
+//   get getMonth(){
+//     return months
+//   }
 }
--- src/store/month.js
+++ src/store/month.js
@@ -1,37 +1,37 @@
-import Day from './day';
+// import Day from './day';
 
-export default class Month {
-  constructor(props) {
-    this.configs = props.configs;
-    this.events = props.events;
-    this.isCurrent = props.isCurrent;
-    this.monthDays = this.getMonthDays;
-    this.monthNumber = props.monthNumber;
-    this.name = props.name;
-    this.numOfDays = props.numOfDays;
-    // this.year = props.year;
-  }
+// export default class Month {
+//   constructor(props) {
+//     this.configs = props.configs;
+//     this.events = props.events;
+//     this.isCurrent = props.isCurrent;
+//     this.monthDays = this.getMonthDays;
+//     this.monthNumber = props.monthNumber;
+//     this.name = props.name;
+//     this.numOfDays = props.numOfDays;
+//     // this.year = props.year;
+//   }
 
-  get getMonthDays() {
-    const monthDays = [];
-    for (let d = 1; d <= 31; d += 1) {
-      monthDays.push(
-        new Day({
-          configs: {},
-          dateInMonth: d,
-          events: [],
-          isCurrent: false,
-          isSelected: false,
-          month: [
-            this.monthNumber,
-            this.name,
-          ],
-          name: 'Monday',
-          year: 2022,
-        }),
-      );
-      console.log(d);
-    }
-    return monthDays;
-  }
-}
+//   get getMonthDays() {
+//     const monthDays = [];
+//     for (let d = 1; d <= 31; d += 1) {
+//       monthDays.push(
+//         new Day({
+//           configs: {},
+//           dateInMonth: d,
+//           events: [],
+//           isCurrent: false,
+//           isSelected: false,
+//           month: [
+//             this.monthNumber,
+//             this.name,
+//           ],
+//           name: 'Monday',
+//           year: 2022,
+//         }),
+//       );
+//       console.log(d);
+//     }
+//     return monthDays;
+//   }
+// }
